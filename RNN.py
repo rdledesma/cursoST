878fd0b766f4100a2e6efcfcb76a4853befc74bb
rdledesma <rdledesma1995@gmail.com>
2024-06-22 11:44:51 -0300

    add changes inform

--- RNN.py
+++ RNN.py
@@ -5,12 +5,23 @@ from keras.layers import LSTM, Dense
 import Metrics as m
 import joblib
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 dTrain = pd.read_csv('process/train.csv')
 dTrain['GHI'] = dTrain.GHI * 60
-dTrain['DHI'] = dTrain.DHI * 60
 
-X = dTrain[['GHI','DHI','AOD OR','Cloud coverage']].values
+varsRegs = ['TOA', 'Clear sky GHI', 'Clear sky BHI', 'Clear sky DHI',
+       'Clear sky BNI', 'GHI', 'BHI', 'DHI', 'BNI', 'Reliability', 'sza',
+       'summer/winter split', 'tco3', 'tcwv', 'AOD BC', 'AOD DU', 'AOD SS',
+       'AOD OR', 'AOD SU', 'AOD NI', 'AOD AM', 'AOD SO', 'Snow probability',
+       'fiso', 'fvol', 'fgeo', 'albedo', 'Cloud coverage', 'Cloud type']
+
+
+
+
+
+
+X = dTrain[varsRegs].values
 y = dTrain.ghi.values
 
 scaler = StandardScaler()
@@ -21,20 +32,52 @@ joblib.dump(scaler, 'models/scaler.joblib')
 
 
 
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+
+
+
 
 # define the model architecture
 model = Sequential()
 model.add(Dense(2,  input_shape=(X_scaled.shape[1], ), activation='linear'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(16, activation='relu'))
-model.add(Dense(1, activation='linear'))
+model.add(Dense(1, activation='relu'))
 
 
 print(model.summary())
 
 model.compile(loss='mse', optimizer='adam')# train the model
 
-model.fit(X_scaled, y, batch_size=100, epochs=50)
+
+
+
+
+# Ajustar el modelo y registrar la pérdida
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), batch_size=100, epochs=50)
+
+# Graficar la función de pérdida
+plt.figure(figsize=(10, 5))
+plt.plot(history.history['loss'], label='Pérdida de Entrenamiento')
+plt.plot(history.history['val_loss'], label='Pérdida de Validación')
+plt.xlabel('Épocas')
+plt.ylabel('Pérdida (MSE)')
+plt.title('Función de Pérdida durante el Entrenamiento y la Validación')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
 
 joblib.dump(model, 'models/MLP.joblib')
 
